19de525616c2afb2115d5c994c419790f00e38d3
Raphael Vispo <rsvispo@up.edu.ph>
2022-10-07 00:55:14 +0000

    feat: add create todo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,50 @@
 import Fastify from 'fastify';
+import { getTodos, saveTodos } from './utils/db/index.js';
+import { v4 } from 'uuid';
+
+const prefix = '/api';
 
 export async function build () {
   const fastify = Fastify({ logger: true });
 
-  fastify.get('/api', async (request, reply) => {
+  fastify.get(prefix, async (request, reply) => {
     return { success: true };
+  });
+
+  fastify.post(`${prefix}/todo`, async (request, reply) => {
+    const { body } = request;
+    const { title, description, isDone = false } = body;
+    const db = await getTodos();
+
+    const id = v4();
+
+    const todo = {
+      title,
+      description,
+      isDone,
+      createdDate: new Date().getTime(),
+      updatedDate: new Date().getTime()
+    };
+
+    db.todos[id] = todo;
+
+    await saveTodos(db);
+
+    /**
+     * const newObj = {
+     *   id
+     * }
+     *
+     * for (const key in todo) {
+     *   newObj[key] = todo[key]
+     * }
+     *
+     * return newObj
+     */
+    return {
+      id,
+      ...todo
+    };
   });
 
   return fastify;
