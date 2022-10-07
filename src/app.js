eea0dd2ac2023a6999d217ba1bbf11d1387bd828
Raphael Vispo <rsvispo@up.edu.ph>
2022-10-07 16:06:19 +0000

    feat: get many todo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import Fastify from 'fastify';
-
+import { getManyTodos } from './services/todos/get-many-todo.js';
 import { createTodo } from './services/todos/create-todo.js';
 import { general } from './services/general/index.js';
 
@@ -10,7 +10,11 @@ export async function build () {
 
   fastify.get(prefix, general);
 
+  // creating todo
   fastify.post(`${prefix}/todo`, createTodo);
+
+  // get many to do
+  fastify.get(`${prefix}/todo`, getManyTodos);
 
   return fastify;
 }
---
+++ test/todo/get-many-todo.test.js
@@ -0,0 +1,44 @@
+import tap from 'tap';
+import 'must/register.js';
+
+import { build } from '../../src/app.js';
+
+tap.mochaGlobals();
+
+const prefix = '/api';
+
+describe('Get many to todo should work', async () => {
+  let app;
+
+  before(async () => {
+    app = await build();
+  });
+  it('should return a list of objects  with the default limit', async () => {
+    const respose = await app.inject({
+      method: 'GET',
+      url: `${prefix}/todo`
+
+    });
+
+    respose.statusCode.must.be.equal(200);
+
+    const result = await respose.json();
+
+    // expect that id exist
+    result.length.must.not.be.above(5);
+  });
+
+  it('should return a list of objects with default limit', async () => {
+    const respose = await app.inject({
+      method: 'GET',
+      url: `${prefix}/todo?limit=2`
+
+    });
+
+    respose.statusCode.must.be.equal(200);
+    const result = await respose.json();
+
+    // expect that id exist
+    result.length.must.not.be.above(2);
+  });
+});
