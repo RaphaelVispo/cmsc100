92bbe04ffcd94a4e1fc5c06b3aa249ee206f6a5c
Raphael Vispo <rsvispo@up.edu.ph>
2022-10-21 14:11:32 +0000

    feat: add logout and checking cookies

---
+++ src/security/index.js
@@ -0,0 +1,26 @@
+
+import { getTodos } from '../utils/db/index.js';
+
+export class Security {
+  constructor (app) {
+    this.app = app;
+  }
+
+  async cookieAuth (request, reply) {
+    try {
+      const token = request.session.get('token');
+      const { username } = await this.app.jwt.verify(token);
+
+      const db = await getTodos();
+
+      if (!db.users[username]) {
+        return reply.unauthorized();
+      }
+
+      request.username = username;
+    } catch (error) {
+      console.error(error);
+      return reply.unauthorized();
+    }
+  }
+}
